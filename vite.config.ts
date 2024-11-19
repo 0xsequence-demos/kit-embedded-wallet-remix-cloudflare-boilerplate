990d27dd431760607681a4b029f109a575efde38
Matt Rintoul <matt@sayyeah.com>
2024-11-19 12:45:44 -0500

    use npm

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import {
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 declare module "@remix-run/cloudflare" {
   interface Future {
     v3_singleFetch: true;
@@ -13,10 +13,6 @@ declare module "@remix-run/cloudflare" {
 
 export default defineConfig({
   plugins: [
-    nodePolyfills({
-      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-      include: ["crypto", "events", "util", "stream", "vm"],
-    }),
     remixCloudflareDevProxy(),
     remix({
       future: {
@@ -32,29 +28,5 @@ export default defineConfig({
   // Add noExternal to allow kit to be bundled
   ssr: {
     noExternal: ["@0xsequence/kit"],
-  },
-  resolve: {
-    alias: {
-      fs: "node:fs",
-      buffer: "buffer/",
-      stream: "node:stream",
-      http: "node:http",
-      https: "node:https",
-      "fs/promises": "node:fs/promises",
-      zlib: "node:zlib",
-      os: "node:os",
-      path: "node:path",
-      process: "node:process",
-      net: "node:net",
-      tls: "node:tls",
-      child_process: "node:child_process",
-      url: "node:url",
-      crypto: "crypto",
-      events: "events",
-      util: "util",
-      "node:crypto": "crypto",
-      "node:events": "events",
-      "node:util": "util",
-    },
   },
 });
